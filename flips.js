7af3273c15aa13b62f71fb1fd20ccfef90017bbe
Max Hu <max@12450.top>
2022-02-14 14:59:40 -0500

    Update flips.js

--- flips.js
+++ flips.js
@@ -6,9 +6,5 @@ const args = minimist(process.argv.slice(2));
 const flip = parseInt(args['number']) || 1;
 
 const result = coinFlips(flip);
-if (flip === 1) {
-  console.log(result);
-} else {
-  console.log(result);
-  console.log(countFlips(result));
-}
+console.log(result);
+console.log(countFlips(result));
