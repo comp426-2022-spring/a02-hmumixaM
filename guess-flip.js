770bc5369b9c31075815fcdae8683ab3d2eeb3de
Max Hu <max@12450.top>
2022-02-14 14:43:11 -0500

    Update guess-flip.js

--- guess-flip.js
+++ guess-flip.js
@@ -4,5 +4,7 @@ import minimist from 'minimist';
 const args = minimist(process.argv.slice(2));
 const guess = args['call'] || console.log('Error: no input.');
 
-if (guess) console.log(flipACoin(guess));
- 
+if (!guess || guess !== 'heads' || guess != 'tails') {
+ console.log('Error: wrong input.)
+ console.log('Usage: node guess-flip --call=[heads|tails]')
+} else console.log(flipACoin(guess));
